a7ad982029cfd2f2ccfec3367ba9cc485afef5e8
bindu-yogesh <binduyogesh87@gmail.com>
2026-09-24 16:57:17 +0530

    feat:add expenseviewmodelfactory

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -3,45 +3,34 @@ package com.example.expensetracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.example.expensetracker.data.local.ExpenseDatabase
+import com.example.expensetracker.data.repository.ExpenseRepository
 import com.example.expensetracker.ui.theme.ExpenseTrackerTheme
+import com.example.expensetracker.viewmodel.ExpenseViewModel
+import com.example.expensetracker.viewmodel.ExpenseViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+    private val database by lazy {
+        ExpenseDatabase.getDatabase(applicationContext)
+    }
+
+    private val repository by lazy {
+        ExpenseRepository(database.expenseDao())
+    }
+
+    private val viewModel: ExpenseViewModel by viewModels {
+        ExpenseViewModelFactory(repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             ExpenseTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // UI will come here
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpenseTrackerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/expensetracker/veiwmodel/ExpenseViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.example.expensetracker.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.expensetracker.data.repository.ExpenseRepository
+
+class ExpenseViewModelFactory(
+    private val repository: ExpenseRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ): T {
+        if (modelClass.isAssignableFrom(ExpenseViewModel::class.java)) {
+            return ExpenseViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
